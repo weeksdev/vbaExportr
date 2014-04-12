c3c62c444252c9aa012b2b47dc93184607afead8
Andrew Weeks <adweeks@gmail.com>
2014-04-12 12:59:50 -0600

    Initial Commit

---
+++ vbaExportr/Export.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using VBA = Microsoft.Vbe.Interop;
+
+namespace vbaExportr
+{
+    
+    public static class Export
+    {
+        public class Reference
+        {
+            public bool isBuiltIn { get; set; }
+            public string description { get; set; }
+            public string fullPath { get; set; }
+            public string guid { get; set; }
+            public bool isBroken { get; set; }
+            public int major { get; set; }
+            public int minor { get; set; }
+            public string name { get; set; }
+        }
+        public class ResponseObject
+        {
+            public string projectName { get; set; }
+            public List<Reference> references { get; set; }
+            public bool isProtected { get; set; }
+            public List<BasFile> basFiles { get; set; }
+        }
+        public class BasFile
+        {
+            public string componentName { get; set; }
+            public string code { get; set; }
+        }
+        
+        public static List<ResponseObject> Open(string fileName)
+        {
+            List<ResponseObject> responseObjects = new List<ResponseObject>();
+            var excel = new Excel.Application();
+            excel.DisplayAlerts = false;
+            Console.WriteLine("Opening Workbook...");
+            var workbook = excel.Workbooks.Open(fileName, false, true, Type.Missing, Type.Missing, Type.Missing, true, Type.Missing, Type.Missing, false, false, Type.Missing, false, true, Type.Missing);
+
+            int projectCount = excel.VBE.VBProjects.Count;
+            for (int i = 1; i <= projectCount; i++)
+            {
+                Console.WriteLine("Inspecting VBProject...");
+                ResponseObject responseObj = new ResponseObject();
+                List<BasFile> files = new List<BasFile>();
+                //var project = workbook.VBProject;
+                //var projects = project.Collection.VBE.VBProjects;
+                var project = excel.VBE.VBProjects.Item(i);
+                var projectName = project.Name;
+                responseObj.projectName = projectName;
+
+                List<Reference> references = new List<Reference>();
+                //get all references in app
+                for (int q = 1; q <= project.References.Count; q++)
+                {
+                    var reference = project.References.Item(q);
+                    references.Add(new Reference()
+                    {
+                        isBuiltIn = reference.BuiltIn,
+                        description = reference.Description,
+                        fullPath = reference.FullPath,
+                        guid = reference.Guid,
+                        isBroken = reference.IsBroken,
+                        major = reference.Major,
+                        minor = reference.Minor,
+                        name = reference.Name
+                    });
+                }
+                responseObj.references = references;
+                //get all code modules and mark instead if it was protected and this couldn't be accomplished.
+                if (project.Protection == VBA.vbext_ProjectProtection.vbext_pp_locked)
+                {
+                    responseObj.isProtected = true;
+                }
+                else
+                {
+                    foreach (var component in project.VBComponents)
+                    {
+                        VBA.VBComponent vbComponent = component as VBA.VBComponent;
+                        if (vbComponent != null)
+                        {
+                            string componentName = vbComponent.Name;
+                            var basFile = new BasFile();
+
+                            basFile.componentName = componentName;
+
+                            var componentCode = vbComponent.CodeModule;
+                            int componentCodeLines = componentCode.CountOfLines;
+
+                            int line = 1;
+
+                            string moduleCode = "'No code commited to file.";
+
+                            if (componentCodeLines > 0)
+                            {
+                                moduleCode = componentCode.get_Lines(line, componentCodeLines);
+                            }
+                            basFile.code = moduleCode;
+                            files.Add(basFile);
+
+                        }
+                    }
+                }
+                responseObj.basFiles = files;
+                responseObjects.Add(responseObj);
+            }
+
+            workbook.Close(false);
+            excel.Quit();
+            excel = null;
+            return responseObjects;
+        }
+    }
+}
---
+++ vbaExportr/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using Microsoft.Win32;
+
+namespace vbaExportr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.exportFlyout.IsOpen = false;
+            //var testExcel = Export.Open(@"C:\Users\WeeksDev\Documents\Book1.xlsm");
+        }
+
+        public class ExcelFile
+        {
+            public List<Export.ResponseObject> Projects { get; set; }
+            public string fileName { get; set; }
+            public string filePath { get; set; }
+        }
+
+        ExcelFile excelFile = new ExcelFile();
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.Filter = "Excel Files (*.xlsm,*.xls)|*.xlsm;*.xls";
+            ofd.ShowDialog();
+            this.uploadFileFld.Text = ofd.FileName;
+            if (ofd.FileName != "")
+            {
+                this.exportFlyout.IsOpen = true;
+                this.excelFile.Projects = Export.Open(ofd.FileName);
+                this.excelFile.fileName = ofd.FileName.Split('\\').Last();
+                this.fileNameFld.Content = "File Name: " + this.excelFile.fileName;
+                this.excelFile.filePath = ofd.FileName;
+                this.numberOfProjectsFld.Content = "Project Count: " + this.excelFile.Projects.Count;
+                int totalBasFiles = 0;
+                bool protectedContent = false;
+                foreach (var project in this.excelFile.Projects)
+                {
+                    if (project.isProtected)
+                        protectedContent = true;
+                    foreach (var file in project.basFiles)
+                    {
+                        totalBasFiles += 1;
+                    }
+                }
+                this.numberOfModules.Content = "Module Count: " + totalBasFiles;
+                if (protectedContent)
+                    System.Windows.MessageBox.Show("Protected content detected. Please remove protection to extract content.");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+            dlg.RootFolder = Environment.SpecialFolder.Personal;
+            dlg.Description = "Please select extract folder...";
+            dlg.ShowDialog();
+            string selectedPath = dlg.SelectedPath;
+            if (selectedPath == "")
+            {
+                System.Windows.MessageBox.Show("No path selected.");
+            }
+            else 
+            {
+                foreach (var project in this.excelFile.Projects)
+                {
+                    foreach (var file in project.basFiles)
+                    {
+                        System.IO.File.WriteAllText(System.IO.Path.Combine(selectedPath + "\\" + project.projectName + "." + file.componentName + ".bas"), file.code);
+                    }
+                }
+                if (this.includeExcelFile.IsChecked==true)
+                    System.IO.File.Copy(this.excelFile.filePath, System.IO.Path.Combine(selectedPath + "\\" + this.excelFile.fileName), true);
+            }
+        }
+    }
+}
